df8a48f510f3c9f5b9aaaad869c6b57ae09cca29
Michael Crump <mbcrump29@gmail.com>
2013-04-22 13:29:25 -0500

    demo

---
+++ WindowsPhone/RadHubTileWP8/README.md
@@ -0,0 +1,12 @@
+# RadHubTile Windows Phone 8 
+========================================
+
+Michael Crump
+
+Web: http://telerik.com
+
+Blog: http://blogs.telerik.com/michaelcrump/posts.aspx
+
+Personal Blog: http://michaelcrump.net
+
+Twitter: http://www.twitter.com/mbcrump
---
+++ WindowsPhone/RadHubTileWP8/RadHubTileWP8/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Telerik.Windows.Controls;
+using System.Windows.Media.Imaging;
+
+namespace RadHubTileWP8
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+            Loaded += MainPage_Loaded;
+        }
+
+        void MainPage_Loaded(object sender, RoutedEventArgs e)
+        {           
+                
+        }
+
+        private void rhtFindMyTrip_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            MessageBox.Show("Tapped on Find My Trip");
+        }
+    }
+}
